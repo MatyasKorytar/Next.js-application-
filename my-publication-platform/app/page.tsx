bfea1c49a170e06e5dde46e1fa872500c28f976e
Matyáš Korytar <matyas.korytar.021@pslib.cz>
2025-02-10 21:18:20 +0100

    jebutos

--- my-publication-platform/app/page.tsx
+++ my-publication-platform/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+
+import { GoogleAnalytics } from "nextjs-google-analytics";
 import React from "react";
 import { SessionProvider } from "next-auth/react";
 import Dashboard from "@/pages/dashboard";
@@ -7,6 +9,7 @@ import Dashboard from "@/pages/dashboard";
 export default function Page() {
   return (
     <SessionProvider>
+      <GoogleAnalytics trackPageViews />
       <Dashboard />
     </SessionProvider>
   );
